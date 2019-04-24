d2731cdf1623829145adfa7be4b1f0e8f304aa93
Narendra Sisodiya <narendra.sisodiya@rivigo.com>
2019-04-24 19:34:27 +0530

    Added iframes

--- src/main.js
+++ src/main.js
@@ -5,5 +5,17 @@ function component() {
   element.innerHTML = _.join(['Project Maitri', 'Started'], ' ');
   return element;
 }
+function createIframe(src) {
+  var frame = document.createElement('iframe');
+  frame.src = src;
 
+  frame.onload = function() {
+    // If this is a Chrome content script, contentWindow is offlimits.
+    frame.contentWindow.postMessage('A Message!', "https://<the mothership's domain>");
+  };
+
+  document.body.appendChild(frame);
+}
 document.body.appendChild(component());
+createIframe('http://localhost:3000');
+createIframe('http://localhost:4200');
