0ecc196928ad953226d63d2bf5c66d59d5a20a57
promil <promil.bhardwaj@vyom.com>
2019-04-25 20:05:37 +0530

    Angular project bootstrapped with routes

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './componenets/home/home.component';
+import { AboutComponent } from './componenets/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componenets/home/home.component';
+import { AboutComponent } from './componenets/about/about.component';
+import { HeaderComponent } from './componenets/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
