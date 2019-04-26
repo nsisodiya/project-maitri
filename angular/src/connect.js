bf9f1b8a8095bb6c480eafaf1c02f518fa23233c
Narendra Sisodiya <narendra.sisodiya@rivigo.com>
2019-04-26 16:50:04 +0530

    Added bridge in angular code too

---
+++ angular/src/connect.js
@@ -0,0 +1,16 @@
+import UniBus from './../UniBus';
+
+const bridge = new UniBus();
+
+bridge.on('FROM_Parent', function(param1, param2) {
+  console.log('Message received from Parent', typeof param1, param1, param2);
+});
+
+bridge.on('FROM_IFRAME2', function(param1, param2) {
+  console.log('This was published and catched inside iframe2', typeof param1, param1, param2);
+});
+
+window.setTimeout(function() {
+  bridge.emit('FROM_IFRAME2', { from: 'child2', val: '187' }, { s: 8 });
+  bridge.emit('FROM_IFRAME2', { from: 'child2', val: '188' }, { s: 9 });
+}, 2000);
